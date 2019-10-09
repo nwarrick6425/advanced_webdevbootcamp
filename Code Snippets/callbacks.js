851df308251a99ef52e5deb0d8b5a9f71dbdaa0b
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-10-08 23:45:55 -0500

    Rename forEach.js & add findIndex def

---
+++ Code Snippets/callbacks.js
@@ -0,0 +1,15 @@
+function forEach(arr, callback) {
+  for (var i=0; i < arr.length; i++) {
+    callback(arr[i], i, arr);
+  }
+};
+
+function findIndex(arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if(callback(arr[i], i, arr)) {
+      return i;
+    }
+  }
+
+  return -1;
+};
--- Code Snippets/forEach.js
+++
@@ -1,3 +0,0 @@
-function forEach(arr, callback) {
-  
-};
