9b2d334973ff6ed4b827838d7037cebffaecc684
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-10-31 22:07:14 -0500

    Show current todo list from db on initial page load

--- JSON API & SPA/public/app.js
+++ JSON API & SPA/public/app.js
@@ -1,1 +1,14 @@
-alert("Hi from app.js");
+$(document).ready(function() {
+  $.getJSON("/api/todos")
+  .then(addTodos)
+  .catch(err => {
+
+  });
+});
+
+function addTodos(todos) {
+  todos.forEach(todo => {
+    let newTodo = $('<li class="task">' + todo.name + '</li>');
+    $('.list').append(newTodo);
+  })
+};
