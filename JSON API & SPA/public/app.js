9d581116e5dfdaab86fbfad2dcd332ae0b9ca071
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-10-31 22:11:11 -0500

    Add conditional for completed todos

--- JSON API & SPA/public/app.js
+++ JSON API & SPA/public/app.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 function addTodos(todos) {
   todos.forEach(todo => {
     let newTodo = $('<li class="task">' + todo.name + '</li>');
+    if(todo.completed) {
+      newTodo.addClass("done");
+    }
     $('.list').append(newTodo);
   })
 };
