a29aa2a8e1e798753bf4e44e225b4d4be9104110
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-10-31 22:26:00 -0500

    Add create todo functionality

--- JSON API & SPA/public/app.js
+++ JSON API & SPA/public/app.js
@@ -2,16 +2,38 @@ $(document).ready(function() {
   $.getJSON("/api/todos")
   .then(addTodos)
   .catch(err => {
+    console.log(err.message);
+  });
 
+  $('#todoInput').keypress(event => {
+    if(event.which == 13) {
+      createTodo();
+    }
   });
 });
 
 function addTodos(todos) {
   todos.forEach(todo => {
-    let newTodo = $('<li class="task">' + todo.name + '</li>');
-    if(todo.completed) {
-      newTodo.addClass("done");
-    }
-    $('.list').append(newTodo);
+    addTodo(todo);
+  });
+};
+
+function addTodo(todo) {
+  let newTodo = $('<li class="task">' + todo.name + '</li>');
+  if (todo.completed) {
+    newTodo.addClass("done");
+  }
+  $('.list').append(newTodo);
+}
+
+function createTodo() {
+  let userInput = $('#todoInput').val();
+  $.post('/api/todos', {name: userInput})
+  .then(newTodo => {
+    $('#todoInput').val('');
+    addTodo(newTodo);
+  })
+  .catch(err => {
+    console.log(err.message);
   })
 };
