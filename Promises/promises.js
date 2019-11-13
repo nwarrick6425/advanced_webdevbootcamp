513577ef7229d204e4abe1fd361e71b429ee4aef
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-11-12 22:41:14 -0600

    Refactor using async/await

--- Promises/promises.js
+++ Promises/promises.js
@@ -1,26 +1,20 @@
-function getMostFollowers(...usernames) {
+async function getMostFollowers(...usernames) {
   let baseUrl = 'https://api.github.com/users/';
   let urls = usernames.map(username => $.getJSON(baseUrl + username));
-  return Promise.all(urls).then(function(data) {
-    let max = data.sort((a,b) => a.followers < b.followers)[0];
-    return `${max.nam} has the most followers with ${max.followers}`;
-  })
+  let userList = await Promise.all(urls);
+  let max = userList.sort((a,b) => a.followers < b.followers)[0];
+  return `${max.name} has the most followers with ${max.followers}`;
 }
 
-function starWarsString(id) {
+async function starWarsString(id) {
   var str = '';
-  return $.getJSON(`https://swapi.co/api/people/${id}`).then (function(res) {
-    str += `${res.name} is featured in `;
-    let filmData = res.films[0];
-    return $.getJSON(filmData);
-  }).then(function (res) {
-    str += `${res.title}, directed by ${res.director} `;
-    let planetData = res.planets[0];
-    return $.getJSON(planetData);
-  }).then(function(res) {
-    str += `and it takes place on ${res.name}`;
-    return str;
-  }).then(function(finalString) {
-    return finalString;
-  });
+  let results = await $.getJSON(`https://swapi.co/api/people/${id}`)
+  str += `${results.name} is featured in `;
+  let movies = results.films[0];
+  let moreResults = await $.getJSON(movies);
+  str += `${moreResults.title}, directed by ${moreResults.director} `;
+  let planetData = moreResults.planets[0];
+  let finalResults = await $.getJSON(planetData);
+  str += `and it takes place on ${finalResults.name}`;
+  return str;
 }
