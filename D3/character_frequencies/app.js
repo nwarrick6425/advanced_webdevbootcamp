fba9dafc11d4bc7c2eb5065c01eca36c7bb51af0
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-11-21 22:25:57 -0600

    Add word count functionality

--- D3/character_frequencies/app.js
+++ D3/character_frequencies/app.js
@@ -1,63 +1,56 @@
-d3.select("#reset")
-    .on("click", function() {
-      d3.selectAll(".letter")
-        .remove();
+d3.select('#reset')
+  .on('click', function() {
+    d3.selectAll('.letter')
+      .remove();
 
-      d3.select("#phrase")
-          .text("");
+    d3.select('#phrase')
+      .text('');
 
-      d3.select("#count")
-          .text("");
-    });
+    d3.select('#count')
+      .text('');
+  });
 
-d3.select("form")
-    .on("submit", function() {
-      d3.event.preventDefault();
-      var input = d3.select("input");
-      var text = input.property("value");
+d3.select('form')
+  .on('submit', function() {
+    d3.event.preventDefault();
+    let input = d3.select('input');
+    let text = input.property('value');
 
-      var letters = d3.select("#letters")
-                      .selectAll(".letter")
-                      .data(getFrequencies(text), function(d) {
-                        return d.character;
-                      });
+    let letters = d3.select('#letters')
+                    .selectAll(".letter")
+                    .data(getFrequencies(text), d => d.character);
+    
+    letters
+        .classed('new', false)
+      .exit()
+      .remove();
 
-      letters
-          .classed("new", false)
-        .exit()
-        .remove();
+    letters
+      .enter()
+      .append('div')
+        .classed('letter', true)
+        .classed('new', true)
+      .merge(letters)
+        .style('width', '20px')
+        .style('line-height', '20px')
+        .style('margin-right', '5px')
+        .style('height', d => d.count * 20 + 'px')
+        .text(d => d.character);
 
-      letters
-        .enter()
-        .append("div")
-          .classed("letter", true)
-          .classed("new", true)
-        .merge(letters)
-          .style("width", "20px")
-          .style("line-height", "20px")
-          .style("margin-right", "5px")
-          .style("height", function(d) {
-            return d.count * 20 + "px";
-          })
-          .text(function(d) {
-            return d.character;
-          });
+    d3.select('#phrase')
+      .text(`Analysis of: ${text}`);
 
-      d3.select("#phrase")
-          .text("Analysis of: " + text);
-
-      d3.select("#count")
-          .text("(New characters: " + letters.enter().nodes().length + ")");
-
-      input.property("value", "");
-    });
+    input.property('value', '');
+  });
 
 function getFrequencies(str) {
-  var sorted = str.split("").sort();
-  var data = [];
-  for (var i = 0; i < sorted.length; i++) {
-    var last = data[data.length - 1];
-    if (last && last.character === sorted[i]) last.count++;
+  let sorted = str.split('').sort();
+  let data = [];
+
+  for (let i = 0; i < sorted.length; i++) {
+    let last = data[data.length - 1];
+
+    if(last && last.character === sorted[i]) last.count++;
     else data.push({ character: sorted[i], count: 1 });
   }
   return data;
