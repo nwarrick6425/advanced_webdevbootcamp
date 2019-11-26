afdbd37978dab0cd20a59b9b8a9216c5134ee3c9
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-11-25 22:24:24 -0600

    Add Bar Graph functionality for Birth Years

---
+++ D3/D3_SVG_Example/app.js
@@ -0,0 +1,35 @@
+let minYear = birthData[0].year;
+let maxYear = birthData[birthData.length - 1].year;
+
+let width = 600;
+let height = 600;
+let numBars = 12;
+let barPadding = 10;
+let barWidth = width / numBars - barPadding;
+
+d3.select('input')
+    .property('min', minYear)
+    .property('max', maxYear)
+    .property('value', minYear);
+
+d3.select('svg')
+    .attr('width', width)
+    .attr('height', height)
+  .selectAll('rect')
+  .data(birthData.filter(d => d.year === minYear))
+  .enter()
+  .append('rect')
+    .attr('width', barWidth)
+    .attr('height', d => d.births / 2.5e6 * height)
+    .attr('y', d => height - d.births / 2.5e6 * height)
+    .attr('x', (d, i) => (barWidth + barPadding) * i)
+    .attr('fill', 'purple');
+
+d3.select('input')
+    .on('input', function() {
+      let year = +d3.event.target.value;
+      d3.selectAll('rect')
+        .data(birthData.filter(d => d.year === year))
+          .attr('height', d => d.births / 2.5e6 * height)
+          .attr('y', d => height - d.births / 2.5e6 * height)
+    });
