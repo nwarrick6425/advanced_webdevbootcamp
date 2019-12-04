5ba6960ae127d527743bac123b77044c2d79710c
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-12-03 23:07:09 -0600

    Add range input for multiple years

--- D3/D3_Histogram_Example/app.js
+++ D3/D3_Histogram_Example/app.js
@@ -1,12 +1,14 @@
 let width = 600;
 let height = 600;
+let padding = 20;
 let barPadding = 1;
 let minYear = d3.min(birthData, d => d.year);
+let maxYear = d3.max(birthData, d => d.year);
 let yearData = birthData.filter(d => d.year === minYear);
 
 let xScale = d3.scaleLinear()
                .domain([0, d3.max(yearData, d => d.births)])
-               .rangeRound([0, width]);
+               .rangeRound([padding, width - padding]);
 
 let histogram = d3.histogram()
                   .domain(xScale.domain())
@@ -43,3 +45,54 @@ bars
   .attr('y', d => (xScale(d.x1) + xScale(d.x0)) / 2)
   .attr('x', - height + 10)
   .style('alignment-baseline', 'middle');
+
+d3.select('input')
+    .property('min', minYear)
+    .property('max', maxYear)
+    .property('value', minYear)
+    .on('input', function() {
+      let year = +d3.event.target.value;
+      yearData = birthData.filter(d => d.year === year);
+
+      xScale.domain([0, d3.max(yearData, d => d.births)]);
+      histogram.domain(xScale.domain())
+               .thresholds(xScale.ticks());
+      bins = histogram(yearData);
+
+      yScale.domain([0, d3.max(bins, d => d.length)]);
+
+      bars = d3.select('svg')
+               .selectAll('.bar')
+               .data(bins);
+
+      bars
+        .exit()
+        .remove();
+
+      let g = bars
+                .enter()
+                .append('g')
+                  .classed('bar', true);
+
+      g.append('rect');
+      g.append('text');
+
+      g.merge(bars)
+        .select('rect')
+          .attr('x', d => xScale(d.x0))
+          .attr('y', d => yScale(d.length))
+          .attr('height', d => height - yScale(d.length))
+          .attr('width', d => {
+            let width = xScale(d.x1) - xScale(d.x0) - barPadding;
+            return width < 0 ? 0 : width;
+           })
+          .attr('fill', '#9c27b0');
+
+      g.merge(bars)
+          .select('text')
+            .text(d => `${d.x0} - ${d.x1} (bar height: ${d.length})`)
+            .attr('transform', 'rotate(-90)')
+            .attr('y', d => (xScale(d.x1) + xScale(d.x0)) / 2)
+            .attr('x', - height + 10)
+            .style('alignment-baseline', 'middle');
+    })
