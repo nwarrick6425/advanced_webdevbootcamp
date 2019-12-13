f67b5678fc97b10fdae59165c1db1353a523f62c
Nicholai Dudakiw-Warrick <nwarrick6425@gmail.com>
2019-12-12 21:44:45 -0600

    Add inner pie chart for births by quarter

--- D3/D3_Pie_Chart/app.js
+++ D3/D3_Pie_Chart/app.js
@@ -22,13 +22,22 @@ let monthColorScale = d3.scaleOrdinal()
                         .domain(months)
                         .range(d3.schemeCategory20);
 
+let quarterColorScale = d3.scaleOrdinal()
+                          .domain([0,3])
+                          .range(d3.schemeCategory10);
+
 let svg = d3.select('svg')
             .attr('width', width)
             .attr('height', height);
 svg
   .append('g')
-  .attr('transform', `translate(${width / 2}, ${height / 2})`)
-  .classed('chart', true);
+    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+    .classed('chart', true);
+
+svg
+  .append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+    .classed('inner-chart', true);
 
 svg
   .append('text')
@@ -42,9 +51,7 @@ d3.select('input')
   .property('min', minYear)
   .property('max', maxYear)
   .property('value', minYear)
-  .on('input', function () {
-    makeGraph(+d3.event.target.value)
-  });
+  .on('input', () => makeGraph(+d3.event.target.value));
 
 makeGraph(minYear);
 
@@ -55,13 +62,25 @@ function makeGraph(year) {
                .value(d => d.births)
                .sort((a, b) => months.indexOf(a.month) - months.indexOf(b.month));
 
+  let innerArcs = d3.pie()
+                    .value(d => d.births)
+                    .sort((a,b) => a.quarter - b.quarter);
+
   let path = d3.arc()
                .outerRadius(width / 2 - 40)
                .innerRadius(width / 4);
 
+  let innerPath = d3.arc()
+                    .innerRadius(0)
+                    .outerRadius(width / 4);
+
   let outer = d3.select('.chart')
                  .selectAll('.arc')
                  .data(arcs(yearData));
+
+  let inner = d3.select('.inner-chart')
+                .selectAll('.arc')
+                .data(innerArcs(getDataByQuarter(yearData)));
 
   outer
     .enter()
@@ -70,8 +89,30 @@ function makeGraph(year) {
       .attr('fill', d => monthColorScale(d.data.month))
     .merge(outer)
       .attr('d', path);
+
+  inner
+    .enter()
+    .append('path')
+      .classed('arc', true)
+      .attr('fill', d => quarterColorScale(d.data.quarter))
+    .merge(inner)
+      .attr('d', innerPath);
   
   d3.select('.title')
       .text(`Births by months and quarter for year ${year}`);
 
+}
+
+function getDataByQuarter(data) {
+  let quarterTallies = [0, 1, 2, 3].map(n => ({
+    quarter: n, births: 0
+  }));
+
+  for (let i = 0; i < data.length; i++) {
+    let row = data[i];
+    let quarter = Math.floor(months.indexOf(row.month) / 3);
+    quarterTallies[quarter].births += row.births;
+  }
+
+  return quarterTallies;
 }
